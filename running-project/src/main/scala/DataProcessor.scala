f258f3bc4d14224e8a8d6419ee30405a20bcc86b
garou404 <charlesmeyer347@gmail.com>
2025-02-03 10:38:57 +0100

    implementation of year filtering for processing the data, get_runner_proportion_per_country issue solved

--- running-project/src/main/scala/DataProcessor.scala
+++ running-project/src/main/scala/DataProcessor.scala
@@ -11,10 +11,10 @@ object DataProcessor {
         val df_pop_processed = df_pop.withColumnRenamed("country_name", "country").drop("country_code", "year")
         df_runner_processed
         .join(df_pop_processed, "country", "left")
-        .withColumn("runner_proportion", expr("count / population"))
+        .withColumn("runner_proportion", expr("runner_nb_per_country / population"))
         .withColumn("runner_proportion", round(col("runner_proportion"), 6))
         .sort(desc("runner_proportion"))
-        .drop("count", "population")
+        .drop("runner_nb_per_country", "population")
     }
 
     def get_runner_nb_per_country(df_runner : DataFrame) : DataFrame = {
@@ -29,20 +29,16 @@ object DataProcessor {
         .groupBy("age_group", "athlete").count()
         .groupBy("age_group").count()
         .withColumn("fraction", round(col("count") /  sum("count").over(), 2))
-        // df_runner
-        // .groupBy("age_group", "athlete").count()
-        // .groupBy("age_group").count()
-        // .withColumn("fraction", round(col("count") /  sum("count").over(), 2))
     }
 
-    def get_fastest_runners_for_distance(df_runner : DataFrame, distance_threshold : Double, pace_threshold : Double, gender : String) : DataFrame = {
+    def get_fastest_runners_for_distance(df_runner : DataFrame, distance_threshold : Double, pace_threshold : Double, gender : String, limit : Int) : DataFrame = {
         df_runner.filter(col("distance") >= distance_threshold && col("distance") < 1.2*distance_threshold  && col("pace") > pace_threshold && col("gender") === gender)
         .orderBy(col("pace").asc)
-        .limit(3)
+        .limit(limit)
         .withColumn("best distance", lit(distance_threshold))
     }
 
-    def get_all_time_performances(df_runner : DataFrame, df_wr : DataFrame) : DataFrame = {
+    def get_all_time_performances(df_runner : DataFrame, df_wr : DataFrame, limit : Int) : DataFrame = {
         val distances = Seq(0.1, 0.2, 0.4, 0.8, 1, 1.5, 2, 3, 5, 10, 21.0975, 42.195, 50, 100)
         val genders = Seq("M", "F")
         val temp_df = df_runner
@@ -58,7 +54,7 @@ object DataProcessor {
             .select("pace")
             .first()(0)
 
-            get_fastest_runners_for_distance(temp_df, distance, wr.asInstanceOf[Double], gender)
+            get_fastest_runners_for_distance(temp_df, distance, wr.asInstanceOf[Double], gender, limit)
         }))      
         .reduce(_ union _)
         df_selected
--- running-project/src/main/scala/Main.scala
+++ running-project/src/main/scala/Main.scala
@@ -1,24 +1,26 @@
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.SparkSession
-
+import org.apache.spark.sql.functions.col
 
 object Main {
     def main(args: Array[String]) : Unit = {
+        val year = "2020"
         val spark = SparkSessionBuilder.spark
-        val df = DataReader.readCSV(spark, "/home/charles-m/Projects/ASR/CSC5003/project/data/running/run_ww_2019_d.csv")
+        val df = DataReader.readCSV(spark, "/home/charles-m/Projects/ASR/CSC5003/project/data/running/run_ww_"+year+"_d.csv")
         val df_pop = DataReader.readCSV(spark, "/home/charles-m/Projects/ASR/CSC5003/project/data/population/population_processed.csv")
+
         val df_wr = DataReader.readCSV(spark, "/home/charles-m/Projects/ASR/CSC5003/project/data/wr/running_wr.csv")
         val saveTo = "/home/charles-m/Projects/ASR/CSC5003/project/data/processed_data/"
 
-        // DataProcessor.get_runner_prop_per_age_category(df)
+
         // Define processing functions with their corresponding output file names
         val processingSteps = Seq(
-            // ("runner_prop_per_country", DataProcessor.get_runner_proportion_per_country(df, df_pop)),
-            ("runner_nb_per_country", DataProcessor.get_runner_nb_per_country(df)),
-            ("runner_prop_per_age_category", DataProcessor.get_runner_prop_per_age_category(df)),
-            ("all_time_performances", DataProcessor.get_all_time_performances(df, df_wr)),
-            ("per_runner_data", DataProcessor.get_per_runner_data(df)),
-            ("running_frequency_per_country", DataProcessor.get_running_frequency_per_country(df))
+            ("runner_prop_per_country_"+year, DataProcessor.get_runner_proportion_per_country(df, df_pop.filter(col("year") === year))),
+            ("runner_nb_per_country_"+year, DataProcessor.get_runner_nb_per_country(df)),
+            ("runner_prop_per_age_category_"+year, DataProcessor.get_runner_prop_per_age_category(df)),
+            ("all_time_performances_"+year, DataProcessor.get_all_time_performances(df, df_wr, 20)),
+            ("per_runner_data_"+year, DataProcessor.get_per_runner_data(df)),
+            ("running_frequency_per_country_"+year, DataProcessor.get_running_frequency_per_country(df))
         )
 
         // Process and save each DataFrame
